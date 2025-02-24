580070f294700d5975539d6106b493e9fee3bbbf
MinhKhanh614 <167750602+MinhKhanh614@users.noreply.github.com>
2025-02-24 17:22:26 +0700

    Implement I2C Line Follower Sensor functionality with setup, loop, and data retrieval methods

---
+++ examples/I2C_Line_Follower_SensorWithKxnTask_Serial/I2C_Line_Follower_SensorWithKxnTask_Serial.ino
@@ -0,0 +1,30 @@
+#include "Makerlabvn_I2C_Line_Follower_Sensor.h"
+Makerlabvn_I2C_Line_Follower_Sensor LineFollowSensor;
+
+void setup()
+{
+    Serial.begin(9600);
+    LineFollowSensor.setup();
+}
+
+void loop()
+{
+    /****** THỨ TỰ SENSOR ******/
+    /*-------------------------*/
+    /* |TRÁI| 1 2 3 4 5 |PHẢI| */
+    /*-------------------------*/
+
+    Serial.print("Sensor 1 Value: ");
+    Serial.println(LineFollowSensor.getValue(0));
+    // Serial.print("Sensor 2 Value: ");
+    // Serial.println(LineFollowSensor.getValue(1));
+    // Serial.print("Sensor 3 Value: ");
+    // Serial.println(LineFollowSensor.getValue(2));
+    // Serial.print("Sensor 4 Value: ");
+    // Serial.println(LineFollowSensor.getValue(3));
+    // Serial.print("Sensor 5 Value: ");
+    // Serial.println(LineFollowSensor.getValue(4));
+
+    kxnTaskManager.run(millis());
+    delay(100);
+}
--- src/Makerlabvn_I2C_Line_Follower_Sensor.cpp
+++ src/Makerlabvn_I2C_Line_Follower_Sensor.cpp
@@ -1,1 +1,28 @@
+#include "Makerlabvn_I2C_Line_Follower_Sensor.h"
 
+void Makerlabvn_I2C_Line_Follower_Sensor::setup()
+{
+    Wire.begin();
+    kxnTaskManager.add(this);
+    setState(KXN_TASK_STATE_START);
+}
+
+void Makerlabvn_I2C_Line_Follower_Sensor::loop()
+{
+    Wire.requestFrom(SENSOR_ADDRESS, MAKERLABVN_I2C_LINE_FOLLOWER_SENSOR_BUFFER_SIZE);
+    for (int i = 0; i < MAKERLABVN_I2C_LINE_FOLLOWER_SENSOR_BUFFER_SIZE; i++)
+    {
+        readValue[i] = Wire.read();
+    }
+    kDelay(10);
+}
+
+void Makerlabvn_I2C_Line_Follower_Sensor::getData()
+{
+    run(millis());
+}
+
+int Makerlabvn_I2C_Line_Follower_Sensor::getValue(uint8_t index)
+{
+    return readValue[index];
+}
